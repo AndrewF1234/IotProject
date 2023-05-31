ece60fc874770700970a3c76559a5c98ccaf5469
AndrewF1234 <andrewfongcoe4p@gmail.com>
2023-05-31 17:17:52 -0600

    changed graph timing and added download links. Works on board

--- sensor_profile.py
+++ sensor_profile.py
@@ -7,8 +7,23 @@ import adafruit_tsl2561
 import time
 import datetime
 import csv
+import boto3
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+access_key = ''
+secret_key = ''
+bucket_name = 'collecttestexample'
+file_path = '/var/www/html/5-31-2023.csv'
+key_name = ''
+samples = int((1 * 60)/ 5)
+
 try:
-    filename = '4-4-2023.csv'
+    filepath = '/home/raspberry/IotEnvironmentProject/readingValues/'
+    filename = '5-31-2023.csv'
+    filesvg = 'data.svg'
     i2c = busio.I2C(board.SCL, board.SDA)
     light_sensor = adafruit_tsl2561.TSL2561(i2c, 0x29)
     humidity = adafruit_ahtx0.AHTx0(board. I2C())
@@ -16,12 +31,14 @@ try:
     tmpAddress=0x50 # 12c connection
     beta =  2180# represents maximum digital value should be 4096
     humidity.calibrate()
+    s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 except Exception as e:
     print(e)
 
 
-header = ['date', 'Temperature', 'Temperture 2', 'Humidity', 'Light Intensity(Lux)']
-with open(filename, 'a') as f:
+header = ['date', 'Temperature', 'Temperature 2', 'Humidity', 'Light Intensity(Lux)']
+if (not os.path.exists(filepath + filename)):
+    with open(filepath + filename, "w+") as f:
         writer = csv.writer(f)
         writer.writerow(header)
 
@@ -72,10 +89,26 @@ while True:
         print(f"Humidity: {humid}")
         print(f"Light: {light}")
         
-        with open(filename, 'a') as f:
+        with open(filepath + filename, 'a') as f:
             writer = csv.writer(f)
             data = [read_time(), temp, aht20, humid, light]
             writer.writerow(data)
             f.close()
+        #try:
+        #    s3.upload_file(file_path,bucket_name,"data/new.csv")
+        #except Exception as e:
+        #    print("yuh:" + str(e))
         
-        
+        df = pd.read_csv(filepath + filename)
+        samples = min(len(df.index)-1,samples)
+        plt.figure()
+        df['date'] = pd.to_datetime(df['date'], format="mixed")
+        plt.plot(df['date'][-samples:], df['Temperature'][-samples:])
+        plt.plot(df['date'][-samples:], df['Temperature 2'][-samples:])
+        plt.plot(df['date'][-samples:], df['Humidity'][-samples:])
+        plt.plot(df['date'][-samples:], df['Light Intensity(Lux)'][-samples:])
+        plt.legend(["TemperatureOne (C)", "TemperatureTwo (C)", "Humidity (%)", "Lux"],
+                   loc = "lower right")
+        plt.savefig(filepath + filesvg)
+        plt.close()
+        #plt.show()
